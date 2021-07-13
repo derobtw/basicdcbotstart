57410185d944f1254ae5f2d0c8d45c54ff016cd5
DerinAlc <70971796+derobtw@users.noreply.github.com>
2021-07-13 17:27:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,23 +3,23 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!')
 
-@bot.event
+@bot.event 
 async def on_message(message):
-  if message.content == 'Hi':
+  if message.content == 'Hi': #auto response for 'Hi'
     await message.channel.send('Hi!')
   await bot.process_commands(message)
 
 @bot.event
 async def on_ready():
   print('Bot is active!')
-  await bot.change_presence(activity=discord.Game(name="Hi!"))
+  await bot.change_presence(activity=discord.Game(name="Hi!")) #bot's activity status
   
 @bot.command()
 async def ping(ctx):
     embed = discord.Embed()
     embed.title = 'Ping:'
-    embed.description = f"{round(bot.latency * 1000)}ms"
+    embed.description = f"{round(bot.latency * 1000)}ms" 
     embed.color = ctx.author.color
-    await ctx.send(embed=embed)
+    await ctx.send(embed=embed) #sends the active ping
     
 bot.run('ODYyNzY2NDI2MDI4NzY5MzAw.YOdH2Q.UhUP03gE-s4du4lpaxccP2D_bNg')
